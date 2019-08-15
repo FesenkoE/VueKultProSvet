ea28884c2a63a0490d7af32d971dbff318acbd58
FesenkoE <fesenkoe@ukr.net>
2019-08-15 09:14:33 +0300

    add copy notes

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -36,8 +36,8 @@
         </div>
         <div class="col-sm-12 mt-2">
             <div class="row">
-                <button class="btn btn-primary mr-1">Sort by Date</button>
-                <button class="btn btn-primary">Sort by Title</button>
+                <button class="btn btn-primary mr-1" @click="sortByDate">Sort by Date</button>
+                <button class="btn btn-primary" @click="sortByTitle">Sort by Title</button>
             </div>
         </div>
         <div class="col-sm-12">
@@ -45,12 +45,14 @@
                 <div class="card mt-2">
                     <div class="card-body">
                         <button class="close" @click="removeNote(index)">x</button>
+                        <button class="close mr-1" @click="copyNote(index)"><i class="far fa-copy"></i></button>
                         <h4 class="card-title">{{ note.title }}</h4>
                         <h6 class="card-subtitle mb-2 text-muted">
                             {{ note.date }}
                         </h6>
                         <p class="card-text">{{ note.text }}</p>
                         <p class="text-info"> {{ note.email }} </p>
+                        <span class="close">{{ note.description }}</span>
                     </div>
                 </div>
             </div>
@@ -76,13 +78,13 @@
                         title: 'Visited Hawaii',
                         text: 'I loved the beaches and delicious fresh coconuts',
                         email: 'some@gmail.com',
-                        date: new Date(Date.now()).toLocaleString(),
+                        date: '13.08.2019',
                     },
                     {
                         title: 'Visited London',
                         text: 'I loved the beaches and delicious fresh coconuts',
                         email: 'some@gmail.com',
-                        date: new Date(Date.now()).toLocaleString(),
+                        date: '11.08.2019',
                     },
                 ],
                 isButtonDisabled: true,
@@ -99,7 +101,7 @@
                     text,
                     title,
                     email,
-                    date: new Date(Date.now()).toLocaleString(),
+                    date: this.formatDate(),
                 });
                 this.note.title = '';
                 this.note.text = '';
@@ -110,6 +112,34 @@
                 this.notes.splice(index, 1);
                 this.note.count = this.notes.length;
             },
+            copyNote(index) {
+                this.notes.push({
+                    text: this.notes[index].text,
+                    title: this.notes[index].title,
+                    email: this.notes[index].email,
+                    date: this.formatDate(),
+                    description: 'copy'
+                });
+            },
+            sortByTitle() {
+                this.notes.sort(function(a, b) {
+                    if(a.title < b.title) { return -1; }
+                    if(a.title > b.title) { return 1; }
+                    return 0;
+                });
+            },
+            sortByDate() {
+                this.notes.sort(function(a, b) {
+                    if(a.date < b.date) { return -1; }
+                    if(a.date > b.date) { return 1; }
+                    return 0;
+                });
+            },
+            formatDate() {
+                let now = new Date();
+                let month = now.getMonth() < 10 ? '0' + now.getMonth() : now.getMonth();
+                return now.getDate() + '.' + month + '.' + now.getFullYear();
+            }
         },
         created() {
             this.note.count = this.notes.length;
